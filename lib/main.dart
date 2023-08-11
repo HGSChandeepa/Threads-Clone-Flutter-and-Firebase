b4764ca64c5a3ae70e1e115ffd82d132b8ea81cf
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-08-12 04:29:43 +0530

    Auth screen UI and UX

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 Future<void> main() async {
@@ -36,10 +37,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: const ResponsiveLayout(
-        webSecreenLayout: WebScreenlayout(),
-        mobileScreenLayout: MobileScreenLayout(),
-      ),
+      // home: const ResponsiveLayout(
+      //   webSecreenLayout: WebScreenlayout(),
+      //   mobileScreenLayout: MobileScreenLayout(),
+      // ),
+      home: LoginScreen(),
     );
   }
 }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:instagram_clone/screens/register_screen.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/button.dart';
 
 import '../widgets/text_feild.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
   //controllers for the text feilds
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
 
   @override
 
@@ -26,6 +28,8 @@ class _LoginScreenState extends State<LoginScreen> {
     //dispose the controllers
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
+    _userNameController.dispose();
   }
 
   Widget build(BuildContext context) {
@@ -42,7 +46,9 @@ class _LoginScreenState extends State<LoginScreen> {
                 //for the top space
                 Flexible(
                   flex: 1,
-                  child: Container(),
+                  child: Container(
+                    child: null,
+                  ),
                 ),
                 //image for logo
                 SvgPicture.asset(
@@ -51,9 +57,43 @@ class _LoginScreenState extends State<LoginScreen> {
                   height: 64,
                 ),
                 const SizedBox(
-                  height: 64,
+                  height: 30,
                 ),
 
+                //add a profile image\
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.grey[300],
+                      backgroundImage: NetworkImage(
+                          'https://1fid.com/wp-content/uploads/2022/06/cartoon-profile-picture-12-1024x1024.jpg'),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white54,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const IconButton(
+                          onPressed: null,
+                          icon: Icon(
+                            Icons.add_a_photo,
+                            color: Colors.pinkAccent,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(
+                  height: 30,
+                ),
                 //text feild for email
                 TextFeildInput(
                   hintText: 'Enter Email',
@@ -71,24 +111,46 @@ class _LoginScreenState extends State<LoginScreen> {
                   isPassword: true,
                   inputkeyboardType: TextInputType.visiblePassword,
                 ),
+                const SizedBox(
+                  height: 30,
+                ),
+
+                //text feild for username
+                TextFeildInput(
+                  hintText: 'Enter Username',
+                  controller: _userNameController,
+                  isPassword: false,
+                  inputkeyboardType: TextInputType.name,
+                ),
+                //text feild for bio
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFeildInput(
+                  hintText: 'Enter Bio',
+                  controller: _bioController,
+                  isPassword: false,
+                  inputkeyboardType: TextInputType.text,
+                ),
                 //button for login
                 const SizedBox(
                   height: 30,
                 ),
                 CustomButon(
-                  text: 'Log in',
+                  text: 'Register',
                   onPressed: () {},
                   color: Colors.pinkAccent,
                 ),
                 //button for signup to navigate to signup screen
-                const SizedBox(
-                  height: 100,
+                Flexible(
+                  flex: 1,
+                  child: Container(),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text(
-                      'Don\'t have an account?',
+                      'Already have an account?',
                       style: TextStyle(color: Colors.white),
                     ),
                     TextButton(
@@ -96,12 +158,12 @@ class _LoginScreenState extends State<LoginScreen> {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const RegisterScreen(),
+                            builder: (context) => const LoginScreen(),
                           ),
                         );
                       },
                       child: const Text(
-                        'Sign up.',
+                        'Log in.',
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
